0b62b42b9b489d891db44eb48a2fe3ae23138c85
Akiyah <akiya.mizukoshi@gmail.com>
2009-08-07 23:11:19 +0900

    add interval

--- guriguri.js
+++ guriguri.js
@@ -1,21 +1,50 @@
 // version 2009/08/07
 
 var guriguri = {
+ tags: 123,
+
  initialize: function() {
-  var tags = guriguri.getGuriguriTags()
-  for (var i = 0; i < tags.length; i++) {
-   var tag = tags[i]
+  guriguri.tags = guriguri.getGuriguriTags()
+  for (var i = 0; i < guriguri.tags.length; i++) {
+   var tag = guriguri.tags[i]
 
    try{
     var params = JSON.parse(tag.innerHTML)
     guriguri.changeDivTag(tag, params.src, params.height)
    } catch(e) {}
   }
-  setInterval(guriguri.interval)
+  setInterval(guriguri.interval, 1000)
  },
 
  interval: function() {
-  alert(1)
+  for (var i = 0; i < guriguri.tags.length; i++) {
+   var tag = guriguri.tags[i]
+   if (tag.guriguri_automove) {
+
+    var imgtag = tag.guriguri_imgtag
+    var height = tag.guriguri_height
+    var count = tag.guriguri_count
+
+
+    var position = tag.guriguri_position 
+    var direction = tag.guriguri_direction
+
+    position += direction
+    if (position >= count) {
+     position = count - 1
+     direction = -1
+    }
+    if (position < 0) {
+     position = 0
+     direction = 1
+    }
+
+    imgtag.style.marginTop = -(position * height) + "px"
+
+    tag.guriguri_position = position
+    tag.guriguri_direction = direction
+   }
+  }
  },
 
  getGuriguriTags: function() {
@@ -35,6 +64,10 @@ var guriguri = {
   divtag.style.overflow = 'hidden'
   divtag.innerHTML = ''
   divtag.style.height = '0px'
+
+  divtag.guriguri_automove = true
+  divtag.guriguri_position = 0
+  divtag.guriguri_direction = 1
 
   var imgtag = new Image()
   imgtag.src = src
@@ -61,7 +94,9 @@ var guriguri = {
   divtag.style.width = width + 'px'
   divtag.style.height = divtag.guriguri_height + 'px'
 
-  divtag.onmousemove = function(e) {guriguri.mousemove(e, this)}
+  divtag.onmousemove = function(e) { guriguri.mousemove(e, this) }
+  divtag.onmouseover = function(e) { this.guriguri_automove = false }
+  divtag.onmouseout  = function(e) { this.guriguri_automove = true }
  },
 
  mousemove: function(e, obj) {
@@ -80,9 +115,10 @@ var guriguri = {
   if (x >= width) { x = width - 1 }
   if (x < 0) { x = 0 }
 
-  var p = Math.floor(x * count / width) * height
+  var position = Math.floor(x * count / width)
+  imgtag.style.marginTop = -(position * height) + "px"
 
-  imgtag.style.marginTop = -p + "px"
+  obj.guriguri_automove = false; 
+  obj.guriguri_position = position; 
  }
-
 }
